4c4675d2b1c5a7e0783a51015bc789940e7bf02e
Duarte <rafaella.d.d.carvalho@gmail.com>
2020-06-26 14:09:01 -0300

    Commit da resolução completa do desafio do Modulo 7

--- Modulo 7/Desafio_ORM_Query/main.py
+++ Modulo 7/Desafio_ORM_Query/main.py
@@ -1,36 +1,44 @@
+from django.db.models import Q
 from api.models import User, Agent, Event, Group
+from datetime import datetime, timedelta
 
 
 def get_active_users() -> User:
-    """Traga todos os uarios ativos, seu último login deve ser menor que 10 dias """
-    raise NotImplementedError
+    today = datetime.today()
+    active_users_query = User.objects.filter(
+        last_login__gte=(today - timedelta(10)))
+    return active_users_query
 
 
 def get_amount_users() -> User:
-    """Retorne a quantidade total de usuarios do sistema """
-    raise NotImplementedError
+    all_users = User.objects.all()
+    return all_users.count()
 
 
 def get_admin_users() -> User:
-    """Traga todos os usuarios com grupo = 'admin"""
-    raise NotImplementedError
+    all_admins = User.objects.filter(group__name='admin')
+    return all_admins
 
 
 def get_all_debug_events() -> Event:
-    """Traga todos os eventos com tipo debug"""
-    raise NotImplementedError
+    debug_events = Event.objects.filter(level='debug')
+    return debug_events
 
 
 def get_all_critical_events_by_user(agent) -> Event:
-    """Traga todos os eventos do tipo critico de um usuário específico"""
-    raise NotImplementedError
+    critial_events = Event.objects.filter(
+        Q(level='critical') & Q(agent=agent))
+    return critial_events
 
 
 def get_all_agents_by_user(username) -> Agent:
     """Traga todos os agentes de associados a um usuário pelo nome do usuário"""
-    raise NotImplementedError
+    agents_by_user = Agent.objects.filter(user__name=username)
+    return agents_by_user
 
 
 def get_all_events_by_group() -> Group:
     """Traga todos os grupos que contenham alguem que possua um agente que possuem eventos do tipo information"""
-    raise NotImplementedError
+    group_with_information_events = Group.objects.filter(
+        user__agent__event__level='information')
+    return group_with_information_events
