11e0fe1087eacbb62861d859ae2425fc28cd81d2
Duarte <rafaella.d.d.carvalho@gmail.com>
2020-06-26 10:06:37 -0300

    Commit da resolução do desafio do modulo 6 atualizada

--- Modulo 6/desafio_REST_API/api/views.py
+++ Modulo 6/desafio_REST_API/api/views.py
@@ -1,21 +1,28 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from collections import Counter
 
+
 # Create your views here.
 
+# @api_view(['POST', 'GET'])
 @api_view(['POST'])
 def lambda_function(request):
-    request_data = request.data.get('question')
+    '''
+     if request.method == 'GET':
+        return Response({'Error': 'somente é aceito o método POST'})
 
-    counter = Counter()
-    for number in request_data:
-        counter[number] += 1
+    if request.method == ' POST':
+    '''
+        request_data = request.data.get('question')
 
-    solution = []
-    for item in counter.most_common():
-        for _ in range(item[1]):
-            solution.append(item[0])
+        counter = Counter()
+        for number in request_data:
+            counter[number] += 1
 
-    return Response({'solution': solution})
+        solution = []
+        for item in counter.most_common():
+            for _ in range(item[1]):
+                solution.append(item[0])
+
+        return Response({'solution': solution})
