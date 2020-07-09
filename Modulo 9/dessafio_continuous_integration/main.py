2996971407359fd449f519caf11b0c5a2d9d79af
Duarte <rafaella.d.d.carvalho@gmail.com>
2020-07-09 11:15:30 -0300

    Commit da Resolução do desafio do Modulo 9

--- Modulo 9/dessafio_continuous_integration/main.py
+++ Modulo 9/dessafio_continuous_integration/main.py
@@ -1,4 +1,15 @@
 # .travis.yml
 config = """
+language: python
+python:
+    -"2.7"
+    -"3.7"
+    -"pypy"
+    -"pypy3"
+pypy: 
+    -"7.1.1"
+    -"7.1.1-beta0"
+script: 'pytest'
+install: "pip install -r requirements.txt"
 
 """
